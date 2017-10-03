530e331eb2b170886d4fc635cd895d670d486dc9
Gietson <gietson1@gmail.com>
2017-10-03 10:08:46 +0200

    fix travis ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HttpModule } from '@angular/http'
-import { FormsModule } from '@angular/forms'
 
 import {
   AdsListComponent,
